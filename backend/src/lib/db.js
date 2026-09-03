5b649f995f99b64181a5e9592aa717bc24a353db
Naman <namandhaka1234@gmail.com>
2026-09-03 15:12:01 +0530

    signup section code refactoring

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,8 +2,12 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log("MONGODB CONNECTED:", conn.connection.host);
+    const { MONGO_URI } = process.env;
+    if (!MONGO_URI) throw new Error("MONGO_URI is not set");
+
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MONGODB CONNECTED:", conn.connection.host);
+
     } catch (error) {
         console.error("Error connection to MONGODB:", error);
         process.exit(1); // 1 status code means fail, 0 means success
